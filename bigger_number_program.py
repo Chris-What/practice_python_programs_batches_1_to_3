7b817ea405eb45d93ea01a348175c4e0af2d7312
Chris <chriswell.mangahas@gmail.com>
2025-03-11 07:32:43 +0800

    Added folder, along with Batch 1 - Program 1: bigger_number_program

---
+++ bigger_number_program.py
@@ -0,0 +1,9 @@
+#Batch 1 - Program 1
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+if num_1 > num_2:
+    print(f"The bigger number is: {num_1}.")
+else:
+    print(f"The bigger number is: {num_2}.")
