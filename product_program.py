c5b180614ce42103a3310ffb681a9711dd195e1d
Chris <chriswell.mangahas@gmail.com>
2025-03-12 00:00:02 +0800

    Added Batch 1 - Program 4: product_program

---
+++ product_program.py
@@ -0,0 +1,8 @@
+#Batch 1 - Program 4
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+product = num_1 * num_2
+
+print(f"The product of the two numbers is: {product}.")
