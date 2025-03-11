7243cdf60e1d6888032ae25ff1d65e926e3bae86
Chris <chriswell.mangahas@gmail.com>
2025-03-12 00:25:26 +0800

    Added Batch 1 - Project 6: power_program

---
+++ power_program.py
@@ -0,0 +1,8 @@
+#Batch 1 - Program 6
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+power = num_1 ** num_2
+
+print(f"{num_1} raised to the power of {num_2} is: {power}.")
