9851cd0b7d9d986fc1697471ed88ead1a749fabc
Chris <chriswell.mangahas@gmail.com>
2025-03-12 00:51:49 +0800

    Added Batch 1 - Program 9: even_list_program

---
+++ even_list_program.py
@@ -0,0 +1,9 @@
+#Batch 1 - Program 9
+
+even_list = []
+
+for i in range(100):
+    if i % 2 == 0:
+        even_list.append(i)
+
+print(f"The list of even numbers from 0 to 100 are: {even_list}. ")
