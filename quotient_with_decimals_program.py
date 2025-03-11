9435c045f48e184be39d4261282ea6adffee14be
Chris <chriswell.mangahas@gmail.com>
2025-03-12 00:16:00 +0800

    Added Batch 1 - Program 5: quotient_with_decimals_program

---
+++ quotient_with_decimals_program.py
@@ -0,0 +1,8 @@
+#Batch 1 - Program 5
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+quotient = num_1 / num_2
+
+print(f"The quotient of the two numbers is: {quotient}.")
