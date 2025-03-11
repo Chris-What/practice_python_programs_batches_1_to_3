f2ed3a9e442c9e247502c9bc845de95f73686198
Chris <chriswell.mangahas@gmail.com>
2025-03-12 02:36:49 +0800

    Added Batch 2 - Program 3: two_numbers_difference_program

---
+++ two_numbers_difference_program.py
@@ -0,0 +1,8 @@
+#Batch 2 - Program 3
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+difference = num_1 - num_2
+
+print(f"The difference of the two numbers is: {difference}.")
