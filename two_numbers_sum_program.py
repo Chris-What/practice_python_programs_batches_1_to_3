9711f3b1bdff19667787774fb5f918257ecb4aa3
Chris <chriswell.mangahas@gmail.com>
2025-03-11 23:49:41 +0800

    Added Batch 1 - Program 3: two_numbers_sum_program

---
+++ two_numbers_sum_program.py
@@ -0,0 +1,8 @@
+#Batch 1 - Program 3
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+sum = num_1 + num_2
+
+print(f"The sum of the two numbers is: {sum}.")
