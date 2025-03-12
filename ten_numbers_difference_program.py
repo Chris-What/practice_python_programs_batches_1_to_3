60301fc080e4a48212ed75d11f63b6766a5fc11c
Chris <chriswell.mangahas@gmail.com>
2025-03-12 09:38:51 +0800

    Added Batch 2 - Program 6: ten_numbers_difference_program

---
+++ ten_numbers_difference_program.py
@@ -0,0 +1,13 @@
+#Batch 2 - Program 6
+
+num_list = []
+
+for i in range(10):
+    num = float(input(f"Enter number {i + 1}: "))
+    num_list.append(num)
+
+difference = num_list[0]
+for num in num_list[1:]:
+    difference -= num
+
+print(f"The difference of the first number {num_list[0]} minus the rest of the numbers is: {difference}.")
