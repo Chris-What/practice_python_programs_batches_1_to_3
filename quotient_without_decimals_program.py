7178d23a8473b822eee563878d36c67c04067f6f
Chris <chriswell.mangahas@gmail.com>
2025-03-12 19:37:51 +0800

    Updated programs involving division to display a message when attempting to divide by zero

--- quotient_without_decimals_program.py
+++ quotient_without_decimals_program.py
@@ -3,6 +3,8 @@
 num_1 = float(input("Enter first number: "))
 num_2 = float(input("Enter second number: "))
 
-quotient = int(num_1 // num_2)
-
-print(f"The quotient of the two numbers, without decimal values, is: {quotient}.")
+if num_2 == 0:
+    print("Undefined; cannot divide by zero.")
+else:
+    quotient = int(num_1 // num_2)
+    print(f"The quotient of the two numbers, without decimal values, is: {quotient}.")
--- remainder_program.py
+++ remainder_program.py
@@ -3,6 +3,8 @@
 num_1 = float(input("Enter first number: "))
 num_2 = float(input("Enter second number: "))
 
-remainder = num_1 % num_2
-
-print(f"The remainder of {num_1} divided by {num_2} is: {remainder}.")
+if num_2 == 0:
+    print("Undefined; cannot divide by zero.")
+else:
+    remainder = num_1 % num_2
+    print(f"The remainder of {num_1} divided by {num_2} is: {remainder}.")
