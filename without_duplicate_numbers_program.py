7b391a959b089a80e668fcda2781c7ea158350ed
Chris <chriswell.mangahas@gmail.com>
2025-03-12 18:40:52 +0800

    Updated without_duplicate_numbers_program to be simpler

--- without_duplicate_numbers_program.py
+++ without_duplicate_numbers_program.py
@@ -6,9 +6,8 @@ for i in range(10):
     num = float(input(f"Enter number {i + 1}: "))
     num_list.append(num)
 
-unique_num = [num for num in num_list if num_list.count(num) == 1]
-
 print("Out of the ten numbers entered, the number/s that doesn't/don't have a duplicate is/are:")
 
-for num in unique_num:
-    print(num)
+for num in num_list:
+    if num_list.count(num) == 1:
+        print(num)
