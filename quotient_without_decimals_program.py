a32bb578f2ca763b802ba61229dea43c3ebff629
Chris <chriswell.mangahas@gmail.com>
2025-03-12 09:04:45 +0800

    Added Batch 2 - Program 4: quotient_without_decimals_program

---
+++ quotient_without_decimals_program.py
@@ -0,0 +1,8 @@
+#Batch 2 - Program 4
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+quotient = int(num_1 // num_2)
+
+print(f"The quotient of the two numbers, without decimal values, is: {quotient}.")
