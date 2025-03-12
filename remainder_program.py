8cabd9d663c6f446eb410d1aed69857bb36a809f
Chris <chriswell.mangahas@gmail.com>
2025-03-12 09:19:56 +0800

    Added Batch 2 - Program 5: remainder_program

---
+++ remainder_program.py
@@ -0,0 +1,8 @@
+#Batch 2 - Program 5
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+remainder = num_1 % num_2
+
+print(f"The remainder of {num_1} divided by {num_2} is: {remainder}.")
