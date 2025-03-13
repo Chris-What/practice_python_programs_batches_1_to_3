28a96263630a0fedb45901815522f90480264077
Chris <chriswell.mangahas@gmail.com>
2025-03-13 17:02:53 +0800

    Added Batch 4 - Program 4: highest_to_lowest_numbers_program

---
+++ highest_to_lowest_numbers_program.py
@@ -0,0 +1,16 @@
+#Batch 4 - Program 4
+
+num_list = []
+
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        num_list.append(num)
+
+    except ValueError:
+        print("The list of numbers entered, arranged from highest to lowest, is:")
+
+        num_list.sort(reverse = True)
+        for num in num_list:
+            print(num)
+        break
