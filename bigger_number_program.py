c168a565ed503595929705b86ee3dca9d293dfd2
Chris <chriswell.mangahas@gmail.com>
2025-03-13 18:16:05 +0800

    Fixed grammar on bigger_number_program (unnecessary period)

--- bigger_number_program.py
+++ bigger_number_program.py
@@ -4,6 +4,6 @@ num_1 = float(input("Enter first number: "))
 num_2 = float(input("Enter second number: "))
 
 if num_1 > num_2:
-    print(f"The bigger number is: {num_1}.")
+    print(f"The bigger number is: {num_1}")
 else:
-    print(f"The bigger number is: {num_2}.")
+    print(f"The bigger number is: {num_2}")
