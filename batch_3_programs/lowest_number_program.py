eafd8cf113f99f22f036f00aa815ac241319423d
Chris <chriswell.mangahas@gmail.com>
2025-03-13 22:56:51 +0800

    Renamed folder3 to batch_3_programs

---
+++ batch_3_programs/lowest_number_program.py
@@ -0,0 +1,13 @@
+#Batch 3 - Program 4
+
+lowest = None
+
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        if lowest is None or num < lowest:
+            lowest = num
+    
+    except ValueError:
+        print(f"Out of all the numbers entered, the lowest number is: {lowest}")
+        break
