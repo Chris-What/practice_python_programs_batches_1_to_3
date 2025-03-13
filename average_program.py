63cd733701029fccd45af41935636778d41df4a4
Chris <chriswell.mangahas@gmail.com>
2025-03-13 22:06:22 +0800

    Added a display message in average_program when no number is entered. Changed a variable name from "sum" to "total" to avoid confusion and error. Also fixed grammar (unnecessary period)

--- average_program.py
+++ average_program.py
@@ -1,15 +1,19 @@
 #Batch 4 - Program 5
 
-sum = 0
+total = 0
 num_count = 0
 
 while True:
     try:
         num = float(input("Enter a number: "))
-        sum += num
+        total += num
         num_count += 1
     
     except ValueError:
-        average = sum / num_count
-        print(f"The average of all numbers entered is: {average}.")
-        break
+        if num_count == 0:
+            print("No number entered.")
+            break
+        else:
+            average = total / num_count
+            print(f"The average of all numbers entered is: {average}")
+            break
